9e32231b0c2fafe90f687fdf2194b97d22d92e46
musaubrian <musaubrian15@gmail.com>
2022-11-01 12:36:38 +0300

    Chore: make input less specific on currency

--- btc.py
+++ btc.py
@@ -45,23 +45,18 @@ def convert_to_kes(usd: int):
     converts the USD price to KES
 
     Args::
-        usd(str): btc price in USD
+        usd(int): btc price in USD
     """
     convertion_rate = int(input("Enter current conversion rate(1~1000): "))
-    curr_prefix = input("Currency prefix(USD/KES): ")
-    usd_to_kes = usd * convertion_rate
+    curr_prefix = input("Currency prefix(e.g USD): ")
+    usd_to_curr = usd * convertion_rate
 
     usd_shorthand = usd / 1000
-    kes_shorthand = usd_to_kes / 1000000
+    curr_shorthand = usd_to_curr / 1000000
 
     print(f"\nUSD {usd}\n 1 BTC = USD {usd_shorthand:.2f}K")
-    print(f"{curr_prefix} {usd_to_kes}\n 1 BTC = {curr_prefix} {kes_shorthand:.2f}M")
+    print(f"{curr_prefix} {usd_to_curr}\n 1 BTC = {curr_prefix} {curr_shorthand:.2f}M")
     print("*"*25)
-    if usd < 19000:
-        print(f"\nBUY THE DIP!!!")
-    elif usd > 19000 and usd < 25000:
-        print(f"\nHODLE!!!")
-
 
 
 if __name__ == "__main__":
